6f4e60a6d99ad8ff086d4dad5af5bcebce3fb073
Navtoj Chahal <github@navtoj.com>
2024-05-02 14:07:50 -0700

    Add Custom Font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+	import '@fontsource/outfit/100.css';
+	import '@fontsource/outfit/200.css';
+	import '@fontsource/outfit/300.css';
+	import '@fontsource/outfit/400.css';
+	import '@fontsource/outfit/500.css';
+	import '@fontsource/outfit/600.css';
+	import '@fontsource/outfit/700.css';
+	import '@fontsource/outfit/800.css';
+	import '@fontsource/outfit/900.css';
+
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
 
@@ -7,4 +17,6 @@
 
 <ModeWatcher />
 
-{@render children()}
+<div class="flex h-dvh flex-col">
+	{@render children()}
+</div>
