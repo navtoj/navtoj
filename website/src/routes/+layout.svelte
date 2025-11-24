04f30effaf02eee17ad0941d5353058e46903da0
Navtoj Chahal <github@navtoj.com>
2025-11-23 23:45:50 -0800

    Match System Theme

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.webp';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
@@ -8,5 +9,7 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<ModeWatcher themeColors={{ dark: 'black', light: 'white' }} />
 
 {@render children()}
